7084c82ec0140db89bb906a94955bac6e9a0b0cd
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2024-05-12 20:58:52 +0200

    Update Application.java

--- src/main/java/appConfig/Application.java
+++ src/main/java/appConfig/Application.java
@@ -139,7 +139,7 @@ public class Application {
             ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             ctx.header("Access-Control-Allow-Headers", "Content-Type");
         });
-            return this;
+            return instance;
     }
 
 
